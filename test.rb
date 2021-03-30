739f7b43f99d965c9589fbd0b64cfdd5dda0d2f2
garciajordy <jordygarcia0119@gmail.com>
2021-03-30 19:24:27 +0300

    Add test.rb file to test my linters

---
+++ test.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class Test
+    def initialize(name)
+      @name = [name]
+      @age = name.length
+    end
+  
+    def call_me
+      puts "HEEEEY, #{@name}!!"
+    end
+  end
+  
